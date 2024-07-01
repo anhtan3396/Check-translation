a0f4c97fcaaed994cae51801532dc95b6ff00c15
user <anhtan.le@sociolla.com>
2024-07-01 10:01:48 +0700

    init commit

---
+++ translation-scanproduct.js
@@ -0,0 +1,59 @@
+const SCAN_PRODUCT_TABS_TRANSLATION = {
+  id: {
+    EXAMPLE: "EX",
+    SCAN_PRODUCT: "Scan Produk",
+    SCAN_QR: "Scan QR",
+    SCAN_PRODUCT_SUB:
+      "Scan barcode dari produk makeup dan skincare yang kamu jumpai, dapatkan informasi lengkap tentang produk tersebut beserta review dari user SOCO.",
+    SCAN_QR_SUB:
+      "Scan kode QR untuk mendapatkan rekomendasi produk berdasarkan Beauty Profile kamu di Sociolla Store.",
+    SCAN_TITLE_STEP: "Scan for Pretty benefits",
+    SCAN_BARCODE: "Scan Barcode",
+    SCAN_QRCODE: "Scan QR Code",
+    SCAN_STEP_1: " produk untuk review terlengkap atau ",
+    SCAN_STEP_2:
+      " di Sociolla Store untuk dapatkan rekomendasi & promo cantik yang tersedia!",
+    SCAN_START: "scanning will start automatically",
+    DONE_LONG: "DONE",
+    SKIP_LONG: "SKIP",
+    "SCAN GAGAL": "SCAN GAGAL",
+    "TRY AGAIN": "TRY AGAIN",
+    "Maaf, kode QR tidak valid": "Maaf, kode QR tidak valid",
+    "Barcode tidak dikenali": "Barcode tidak dikenali",
+    "Maaf, terjadi kendala pada teknis atau pada koneksi internet kamu.":
+      "Maaf, terjadi kendala pada teknis atau pada koneksi internet kamu.",
+    "Terima kasih atas feedback-nya": "Terima kasih atas feedback-nya",
+    "Apakah produk ini sudah sesuai dengan yang kamu scan?":
+      "Apakah produk ini sudah sesuai dengan yang kamu scan?",
+  },
+  vn: {
+    EXAMPLE: "EX",
+    SCAN_PRODUCT: "Quét Sản Phẩm",
+    SCAN_QR: "Quét QR",
+    SCAN_PRODUCT_SUB:
+      "Quét mã sản phẩm trang điểm hoặc dưỡng da của bạn để nhận thông tin đầy đủ về sản phẩm và đánh giá từ người dùng SOCO.",
+    SCAN_QR_SUB:
+      "Quét mã QR tại của hàng Sociolla để nhận sản phẩm đề xuất theo Hồ sơ của bạn.",
+    SCAN_TITLE_STEP: "Một camera cho mọi thứ",
+    SCAN_BARCODE: "Quét mã vạch",
+    SCAN_QRCODE: "Quét mã QR",
+    SCAN_STEP_1:
+      " để xem thêm thông tin của sản phẩm và những đánh giá của người dùng Sociolla. ",
+    SCAN_STEP_2:
+      " tại cửa hàng Sociolla để chơi trò chơi và nhận ưu đãi thật hấp dẫn nhé!",
+    SCAN_START: "việc quét mã sẽ diễn ra tự động.",
+    DONE_LONG: "làm xong",
+    SKIP_LONG: "bỏ qua",
+    "SCAN GAGAL": "quét không thành công",
+    "TRY AGAIN": "thử lại",
+    "Maaf, kode QR tidak valid": "Rất tiếc, mã QR không hợp lệ",
+    "Barcode tidak dikenali": "Mã vạch không được công nhận",
+    "Maaf, terjadi kendala pada teknis atau pada koneksi internet kamu.":
+      "Rất tiếc, đã xảy ra sự cố kỹ thuật với kết nối Internet của bạn.",
+    "Terima kasih atas feedback-nya": "Cảm ơn bạn đã phản hồi",
+    "Apakah produk ini sudah sesuai dengan yang kamu scan?":
+      "Sản phẩm này có khớp với những gì bạn đã quét không?",
+  },
+};
+
+export default SCAN_PRODUCT_TABS_TRANSLATION;
